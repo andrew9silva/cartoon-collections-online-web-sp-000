60d5a9727912b8607050a168abb01aecbee3b8f5
Andrew Silva <andrewsilva9@gmail.com>
2019-04-26 01:28:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include? do |word|
+  array.find do |word|
     word == "cheddar" || word == "gouda" || word == "camembert"
   end
 
