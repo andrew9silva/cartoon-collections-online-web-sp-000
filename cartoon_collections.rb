60d8659e4793dc1bc1763ce19775494f4f12a2e1
Andrew Silva <andrewsilva9@gmail.com>
2019-04-26 00:41:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(names)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
