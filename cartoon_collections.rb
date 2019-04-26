e0f5962311f704f332ce2eda0d6404b4da4f231b
Andrew Silva <andrewsilva9@gmail.com>
2019-04-26 01:26:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  array.include?
+  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? do |word|
-    word == cheese_types[0]
-  end
 end
